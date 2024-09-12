2e1feccc83edc4b596670dfe9e89a4ec2a24b413
AlgoWebBot <shimulzahan636@gmail.com>
2024-09-13 03:07:16 +0600

    create dependency injection on auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    loginUser(
+        email: string,
+        password: string
+    ) {
+        return {
+            userId: '123',
+            token: 'abc123',
+            roles: ['admin']
+        }
+    }
+
+}
