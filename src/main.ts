805f633ab6d59165f600906c44137b92ce440071
AlgoWebBot <shimulzahan636@gmail.com>
2024-09-13 15:50:34 +0600

    create validation, enum, dto and post module for blog post here

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,14 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true, //for create dto instatnce
-  })); 
+  }),
+  );
+
+  // create swagger documentation here
+  const config = new DocumentBuilder().setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/posts/dtos/create-post.dto.ts
@@ -0,0 +1,61 @@
+import { IsArray, IsEmpty, IsEnum, IsISO8601, IsJSON, isNotEmpty, IsNotEmpty, IsOptional, IsString, IsUrl, Matches, MinLength, ValidateNested } from "class-validator";
+import { PostStatus } from "../enum/PostStatus.enum";
+import { PostType } from "../enum/postType.enum";
+import { CreateMetaOptionsDto } from "./createMeatOption.dto";
+import { Type } from "class-transformer";
+
+
+export class CreatePostDto {
+
+    @IsString()
+    @MinLength(4)
+    @IsNotEmpty()
+    title: string;
+
+    @IsEnum(PostType)
+    @IsNotEmpty()
+    postType: PostType;
+
+    @IsString()
+    @IsNotEmpty()
+    @Matches(/^[a-z0-9]+(?:-[a-zA-Z]+)*$/, {
+        message: 'Slug should only contain lowercase letters, numbers, and hyphens.'
+    })
+    slug: string;
+
+    @IsEnum(PostStatus)
+    status: PostStatus;
+
+    @IsString()
+    @IsEmpty()
+    content?: string;
+
+    @IsString()
+    @IsOptional()
+    @IsJSON()
+    schema?: string;
+
+    @IsString()
+    @IsOptional()
+    @IsUrl()
+    featuredImageUrl?: string;
+
+    @IsISO8601()
+    @IsOptional()
+    publishOn?: Date;
+
+    @IsOptional()
+    @IsArray()
+    @IsString({ each: true }) //check all value of a array of string
+    @MinLength(3, { each: true })
+    tags?: string[];
+
+
+    // here nested dto works
+    @IsOptional()
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => CreateMetaOptionsDto)
+    metaOptions: CreateMetaOptionsDto[]
+
+}
