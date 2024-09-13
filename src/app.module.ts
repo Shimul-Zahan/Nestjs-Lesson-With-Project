b936a473230cea7c0e68ebe1111dfe160b18a312
AlgoWebBot <shimulzahan636@gmail.com>
2024-09-13 23:56:47 +0600

    finished this project for today

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,19 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '636deadline',
-    database: 'blog-db',
-    entities: [],
-    synchronize: true,
+  imports: [UsersModule, PostsModule, AuthModule, TypeOrmModule.forRootAsync({
+    imports: [],
+    inject: [],
+    useFactory: () => ({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '636deadline',
+      database: 'blog-db',
+      entities: [],
+      synchronize: true,
+    })
   })],
   controllers: [AppController],
   providers: [AppService],
